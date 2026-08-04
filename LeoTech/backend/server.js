18b604c16c1090066ffc153d77b74e2783f87228
leonardo <leonadoxd12@gmail.com>
2026-08-04 12:34:12 -0500

    fix: habilitar soporte de Promises en mysql2 para queries con await

--- LeoTech/backend/server.js
+++ LeoTech/backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
 
